094a4fa53ca18a56cc1eb6d8b08b535a90355897
Sergio <2016@holbertonschool.com>
2021-02-16 11:58:36 -0500

    Delete excess of blank lines

--- models/base_model.py
+++ models/base_model.py
@@ -17,8 +17,6 @@ class BaseModel:
                 if key != '__class__':
                     setattr(self, key, val)
 
-
-
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
