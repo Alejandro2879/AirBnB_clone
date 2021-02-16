abd0fad8219b31eacf32b41eab477ea9a19dfc6b
Sergio <2016@holbertonschool.com>
2021-02-16 11:56:38 -0500

    Add *args and **kwargs to __init__ in BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,46 @@
 #!/usr/bin/python3
-"""[]
+"""[Module define the BaseModel class]
 """
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
+    """[BaseModel class]
+    """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        """[Define initial values to attributes]
+        """
+        if kwargs:
+            for key, val in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, val)
+
+
+
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.today()
-        self.updated_at = datetime.datetime.today()
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
-        return("[{}] ({}) {}".format(BaseModel.__name__, self.id, self.__dict__))
+        """[String format]
+        """
+        return("[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__))
 
     def save(self):
-        self.updated_at = datetime.datetime.today()
+        """[Method to update 'updated_at' attribute]
+        """
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        if self.__dict__['created_at']:
-            self.__dict__['created_at'] = self.__dict__['created_at'].isoformat()
-        if "updated_at" in self.__dict__:
-            self.__dict__['udated_at'] = self.__dict__['updated_at'].isoformat()
-        self.__dict__['__class__'] = BaseModel.__name__
-        return self.__dict__
+        """[Return all key : values of the instance]
+        """
+        new = self.__dict__.copy()
+        if 'created_at' in new:
+            new['created_at'] = new['created_at'].isoformat()
+        if 'updated_at' in new:
+            new['udated_at'] = new['updated_at'].isoformat()
+        new['__class__'] = self.__class__.__name__
+        return new
