3e7ff91df952ede006ae41b4fc36e6ad19d70b98
Sergio <2016@holbertonschool.com>
2021-02-17 15:21:21 -0500

    Add documentation to methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""[]
+"""[Module define the FileStorage class]
 """
 import json
 from models.base_model import BaseModel
@@ -7,13 +7,14 @@ import models
 
 
 class FileStorage:
-    """[]
+    """[serializes instances to a JSON file
+        and deserializes JSON file to instances]
     """
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """[]
+        """[Returns the dictionary __objects]
         """
         if cls is not None:
             new = {}
@@ -24,13 +25,15 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        """[]
+        """[Sets in __objects the obj with key <obj class name>.id]
         """
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
     def save(self):
+        """[Serializes __objects to the JSON file (path: __file_path)]
+        """
         j_obj = {}
         for key in self.__objects:
             j_obj[key] = self.__objects[key].to_dict()
@@ -38,11 +41,12 @@ class FileStorage:
             json.dump(j_obj, doc)
 
     def reload(self):
+        """[Deserializes the JSON file to __objects]
+        """
         try:
             with open(self.__file_path, 'r') as doc:
-                j_obj  = json.load(doc)
+                j_obj = json.load(doc)
             for key in j_obj:
                 self.__objects[key] = j_obj[key]["__class__"](**j_obj[key])
         except:
             pass
-
