6e03d311a50efc5eadccee90c586f52335f0fd07
Sergio <2016@holbertonschool.com>
2021-02-17 18:56:32 -0500

    Add tests for docstring

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,39 +6,47 @@ from datetime import datetime
 import pep8
 import models
 import inspect
-
-
+from unittest import mock
 BaseModel = models.base_model.BaseModel
-doc = models.base_models.__doc__
+doc = models.base_model.__doc__
 
 
-class TestBaseModelDoc(unittest.testCase):
+class TestBaseModelDoc(unittest.TestCase):
     """[Class to test docstring in BaseModel]
     """
+    @classmethod
+    def set_c(self):
+        """[Set]
+        """
+        self.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
+
     def test_pep8(self):
         """[Test pep8]
         """
-        for path in ['models.base_model.py',
-                     'test/test_models/test_base_model.py']:
+        for path in ['models/base_model.py',
+                     'tests/test_models/test_base_model.py']:
             with self.subTest(path=path):
-                err = pycodestyle.Checker(path).check_all()
+                err = pep8.Checker(path).check_all()
                 self.assertEqual(err, 0)
 
     def test_docsting_mod(self):
+        """[Check modules documentation]
+        """
         self.assertIsNot(doc, None, "No docstring for base_model.py")
         self.assertTrue(len(doc) > 1, "No docstring for base_model.py")
 
     def test_docstring_class(self):
+        """[Check class documentation]
+        """
         self.assertIsNot(BaseModel.__doc__, None,
                          "No docstring for BaseModel class")
         self.assertTrue(len(BaseModel.__doc__) > 1,
                         "No docstring for BaseModel class")
 
-    @set_c(self):
-        self.funcs = inspect.getmembers(BaseModel, inspect.isfunction())
-
     def test_docsting_methods(self):
-        for meth in self.funcs:
+        """[Check methods documentation]
+        """
+        for meth in self.base_funcs:
             with self.subTest(function=meth):
                 self.assertIsNot(meth[1].__doc__, None,
                                  "No docstring for {}".format(meth[0]))
