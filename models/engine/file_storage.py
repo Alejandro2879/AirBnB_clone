ce50ee54310bfc3ad8b3f1af45e0395ec0722d74
Sergio <2016@holbertonschool.com>
2021-02-17 13:17:42 -0500

    Modify the reload() method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,12 +2,14 @@
 """[]
 """
 import json
+from models.base_model import BaseModel
+import models
 
 
 class FileStorage:
     """[]
     """
-    __file_path = "file.json"
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
@@ -16,31 +18,31 @@ class FileStorage:
         if cls is not None:
             new = {}
             for key, value in self.__objects.items():
-                if value.__class__.__name__ == cls:
+                if cls == value.__class__ or cls == value.__class__.__name__:
                     new[key] = value
             return (new)
-        else:
-            return (self.__objects)
+        return self.__objects
 
     def new(self, obj):
         """[]
         """
         if obj is not None:
-            FileStorage.__objects[obj.id] = obj
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects[key] = obj
 
     def save(self):
-        to_json = {}
+        j_obj = {}
         for key in self.__objects:
-            to_json[key] = FileStorage.__objects[key].to_dict()
+            j_obj[key] = self.__objects[key].to_dict()
         with open(self.__file_path, 'w') as doc:
-            json.dump(to_json, doc)
+            json.dump(j_obj, doc)
 
     def reload(self):
         try:
             with open(self.__file_path, 'r') as doc:
                 j_obj  = json.load(doc)
-                for key in j_obj:
-                    self.__objects[key] = j_obj[key]
+            for key in j_obj:
+                self.__objects[key] = j_obj[key]["__class__"](**j_obj[key])
         except:
             pass
 
