0f2064f7b0efefd71df28ee287397a0ac9a284a5
Sergio <2016@holbertonschool.com>
2021-02-17 13:16:43 -0500

    Add files __init__.py base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """[Module define the BaseModel class]
 """
-import uuid
-import models
 from datetime import datetime
-
+import models
+import uuid
 
 class BaseModel:
     """[BaseModel class]
@@ -17,6 +16,16 @@ class BaseModel:
             for key, val in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, val)
+            if kwargs.get('created_at', None) and type(self.created_at) is str:
+                self.created_at = kwargs[created_at].isoformat()
+            else:
+                self.created_at = datetime.now()
+            if kwargs.get('updated_at', None) and type(self.updated_at) is str:
+                self.updated_at = kwargs[updated_at].isoformat()
+            else:
+                self.updated_at = kwargs[updated_at].isoformat()
+            if kwargs.get('id', None) is None:
+                self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -32,8 +41,9 @@ class BaseModel:
         """[Method to update 'updated_at' attribute]
         """
         self.updated_at = datetime.now()
-        models.storage.save()
         models.storage.new(self)
+        models.storage.save()
+        
 
     def to_dict(self):
         """[Return all key : values of the instance]
@@ -44,4 +54,6 @@ class BaseModel:
         if 'updated_at' in new:
             new['updated_at'] = new['updated_at'].isoformat()
         new['__class__'] = self.__class__.__name__
+        if '_sa_instance_state' in new:
+            del new['_sa_instance_state']
         return new
