817836fce094a413956e3a2737d96858bff1489c
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-02-18 01:18:17 +0000

    Add do_create | Console

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module contain HBNBCommand class to use the command interpreter"""
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -18,6 +19,23 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, arg):
         'Quit command to exit the program\n'
         return True
+
+    def do_create(self, nameins):
+        """ Create a new instance """
+        nameClas = [
+            "BaseModel"
+        ]
+        if len(nameins) == 0:
+            print("** class name missing **")
+            return
+        for name in nameClas:
+            if name != nameins:
+                print("** class doesn't exist **")
+                return
+        new_instance = BaseModel()
+        print(new_instance.id)
+        return
+        
 
 
 if __name__ == '__main__':
